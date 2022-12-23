b81de1f740dbde3a4af9c3b2a232c4eead6ab099
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-12-22 23:51:45 -0300

    feat: route home (user home page)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import RankingPage from "./pages/RankingPage/RankingPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import UserHomePage from "./pages/UserHomePage/UserHomePage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<UserHomePage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/ranking" element={<RankingPage />} />
---
+++ src/pages/UserHomePage/UserHomePage.js
@@ -0,0 +1,5 @@
+function UserHomePage() {
+  return;
+}
+
+export default UserHomePage;
