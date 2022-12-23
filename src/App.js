187f253893387d3ca3bdc196cddd732e0e9e9e71
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-12-22 22:25:21 -0300

    feat: created routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./assets/styles/GlobalStyle";
+import HomePage from "./pages/HomePage/HomePage";
+import RankingPage from "./pages/RankingPage/RankingPage";
+import SignInPage from "./pages/SignInPage/SignInPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/ranking" element={<RankingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/RankingPage/RankingPage.js
@@ -0,0 +1,6 @@
+function RankingPage() {
+    return;
+  }
+  
+  export default RankingPage;
+  
